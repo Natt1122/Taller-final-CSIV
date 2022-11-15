3577725956051298f9e487250cfdb0f907bf941d
natalia andrea arboleda orozco <natalia.arboleda25@gmail.com>
2022-11-14 20:54:53 -0500

    Trabajo final Construcción de software

---
+++ Final/Final/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TbID = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.BtnEliminar = new System.Windows.Forms.Button();
+            this.BtnBuscar = new System.Windows.Forms.Button();
+            this.BtnModificar = new System.Windows.Forms.Button();
+            this.BtnCrear = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Tbape2 = new System.Windows.Forms.TextBox();
+            this.Tbape1 = new System.Windows.Forms.TextBox();
+            this.Tbnom = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TbID
+            // 
+            this.TbID.Location = new System.Drawing.Point(297, 147);
+            this.TbID.Name = "TbID";
+            this.TbID.Size = new System.Drawing.Size(121, 20);
+            this.TbID.TabIndex = 23;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(151, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "IDENTIFICACIÓN";
+            // 
+            // BtnEliminar
+            // 
+            this.BtnEliminar.Location = new System.Drawing.Point(333, 253);
+            this.BtnEliminar.Name = "BtnEliminar";
+            this.BtnEliminar.Size = new System.Drawing.Size(104, 30);
+            this.BtnEliminar.TabIndex = 21;
+            this.BtnEliminar.Text = "Eliminar";
+            this.BtnEliminar.UseVisualStyleBackColor = true;
+            this.BtnEliminar.Click += new System.EventHandler(this.BtnEliminar_Click);
+            // 
+            // BtnBuscar
+            // 
+            this.BtnBuscar.Location = new System.Drawing.Point(333, 202);
+            this.BtnBuscar.Name = "BtnBuscar";
+            this.BtnBuscar.Size = new System.Drawing.Size(104, 30);
+            this.BtnBuscar.TabIndex = 20;
+            this.BtnBuscar.Text = "Buscar";
+            this.BtnBuscar.UseVisualStyleBackColor = true;
+            this.BtnBuscar.Click += new System.EventHandler(this.BtnBuscar_Click);
+            // 
+            // BtnModificar
+            // 
+            this.BtnModificar.Location = new System.Drawing.Point(157, 253);
+            this.BtnModificar.Name = "BtnModificar";
+            this.BtnModificar.Size = new System.Drawing.Size(104, 30);
+            this.BtnModificar.TabIndex = 19;
+            this.BtnModificar.Text = "Modificar";
+            this.BtnModificar.UseVisualStyleBackColor = true;
+            this.BtnModificar.Click += new System.EventHandler(this.BtnModificar_Click);
+            // 
+            // BtnCrear
+            // 
+            this.BtnCrear.Location = new System.Drawing.Point(157, 202);
+            this.BtnCrear.Name = "BtnCrear";
+            this.BtnCrear.Size = new System.Drawing.Size(104, 30);
+            this.BtnCrear.TabIndex = 18;
+            this.BtnCrear.Text = "Crear";
+            this.BtnCrear.UseVisualStyleBackColor = true;
+            this.BtnCrear.Click += new System.EventHandler(this.BtnCrear_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(127, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "SEGUNDO APELLIDO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(139, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "PRIMER APELLIDO";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "NOMBRE";
+            // 
+            // Tbape2
+            // 
+            this.Tbape2.Location = new System.Drawing.Point(297, 108);
+            this.Tbape2.Name = "Tbape2";
+            this.Tbape2.Size = new System.Drawing.Size(121, 20);
+            this.Tbape2.TabIndex = 14;
+            // 
+            // Tbape1
+            // 
+            this.Tbape1.Location = new System.Drawing.Point(297, 71);
+            this.Tbape1.Name = "Tbape1";
+            this.Tbape1.Size = new System.Drawing.Size(121, 20);
+            this.Tbape1.TabIndex = 13;
+            // 
+            // Tbnom
+            // 
+            this.Tbnom.Location = new System.Drawing.Point(297, 29);
+            this.Tbnom.Name = "Tbnom";
+            this.Tbnom.Size = new System.Drawing.Size(121, 20);
+            this.Tbnom.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 307);
+            this.Controls.Add(this.TbID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.BtnEliminar);
+            this.Controls.Add(this.BtnBuscar);
+            this.Controls.Add(this.BtnModificar);
+            this.Controls.Add(this.BtnCrear);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Tbape2);
+            this.Controls.Add(this.Tbape1);
+            this.Controls.Add(this.Tbnom);
+            this.Name = "Form1";
+            this.Text = "Alumnos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TbID;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button BtnEliminar;
+        private System.Windows.Forms.Button BtnBuscar;
+        private System.Windows.Forms.Button BtnModificar;
+        private System.Windows.Forms.Button BtnCrear;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Tbape2;
+        private System.Windows.Forms.TextBox Tbape1;
+        private System.Windows.Forms.TextBox Tbnom;
+    }
+}
+
---
+++ Final/Final/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Reflection.Emit;
+
+namespace Final
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conexion = new SqlConnection("server=DESKTOP-QMJDLO6; database=Alumnos; integrated security=true");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCrear_Click(object sender, EventArgs e)
+        {
+            conexion.Open();
+
+            String Nom = Tbnom.Text;
+            String Ape1 = Tbape1.Text;
+            String Ape2 = Tbape2.Text;
+            String id = TbID.Text;
+
+            string cadena = "insert into Lista1 (Nombre, Apellido1, Apellido2, ID) values('" + Nom+ "', '" + Ape1 + "','" + Ape2 + "', '" + id + "')";
+            SqlCommand cmd = new SqlCommand(cadena, conexion);
+            cmd.ExecuteNonQuery();
+
+            MessageBox.Show("los datos se guardaron correctamente");
+            Tbnom.Text = "";
+            Tbape1.Text = "";
+            Tbape2.Text = "";
+            TbID.Text = "";
+
+            conexion.Close();
+
+        }
+
+        private void BtnModificar_Click(object sender, EventArgs e)
+        {
+            conexion.Open();
+            String Nom = Tbnom.Text;
+            String Ape1 = Tbape1.Text;
+            String Ape2 = Tbape2.Text;
+            String id = TbID.Text;
+
+
+            string cadena = "update Lista1 set Nombre = '" + Nom + "', Apellido1= '" + Ape1 + "', Apellido2='" + Ape2 + "' where ID= " + id;
+            SqlCommand cmd = new SqlCommand(cadena, conexion);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("los datos se guardaron correctamente");
+            Tbnom.Text = "";
+            Tbape1.Text = "";
+            Tbape2.Text = "";
+            TbID.Text = "";
+            conexion.Close();
+        }
+
+        private void BtnBuscar_Click(object sender, EventArgs e)
+        {
+            conexion.Open();
+            string cod = TbID.Text;
+
+            string cadena = "select Nombre, Apellido1, Apellido2 from Lista1 where ID=" + cod;
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            SqlDataReader registro = comando.ExecuteReader();
+
+            if (registro.Read())
+            {
+                MessageBox.Show ("El Alumno es: "+ registro["Nombre"].ToString() + " " + registro["Apellido1"].ToString() + " " + registro["Apellido2"].ToString());
+                BtnBuscar.Enabled = true;
+            }
+            else
+                MessageBox.Show("No existe el alumno");
+            Tbnom.Text = "";
+            Tbape1.Text = "";
+            Tbape2.Text = "";
+            TbID.Text = "";
+            conexion.Close();
+
+        }
+
+        private void BtnEliminar_Click(object sender, EventArgs e)
+        {
+            conexion.Open();
+            string cod = TbID.Text;
+            string cadena = "delete from Lista1 where ID=" + cod;
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            int cant;
+            cant = comando.ExecuteNonQuery();
+            if (cant == 1)
+            {
+                MessageBox.Show("Se elimino el alumno");
+            }
+            Tbnom.Text = "";
+            Tbape1.Text = "";
+            Tbape2.Text = "";
+            TbID.Text = "";
+        }
+    } 
+}
